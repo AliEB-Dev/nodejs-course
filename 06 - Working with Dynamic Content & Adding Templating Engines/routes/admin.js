f53a333521ef70d187e6930a4092e3534bcaa418
AliEB-Dev <aliasgharebrahimzadef@gmail.com>
2026-08-25 16:49:03 +0330

    Convert HTML views to Pug and add layout

--- 06 - Working with Dynamic Content & Adding Templating Engines/routes/admin.js
+++ 06 - Working with Dynamic Content & Adding Templating Engines/routes/admin.js
@@ -5,7 +5,7 @@ const rootDir = require('../util/path');
 
 const products = [];
 router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','add.html'))
+    res.render('add-product',{pageTitle:'add-product'})
 })
 router.use('/add-product',(req,res,next)=>{
     products.push({title: req.body.title})
