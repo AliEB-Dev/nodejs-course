85e25ddc0887fc643a9a4dd6aa03a1dcefcb929e
AliEB-Dev <aliasgharebrahimzadef@gmail.com>
2026-09-01 14:43:45 +0330

    005 Adding a Product Model

--- 07 - The Model View Controller (MVC)/work with ejs/routes/admin.js
+++ 07 - The Model View Controller (MVC)/work with ejs/routes/admin.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-const ProductController = require('../controllers/products') 
+const ProductController = require('../controllers/products');
+
 
 router.get('/add-product',ProductController.getAddProduct)
 router.use('/add-product',ProductController.postAddProducts)
