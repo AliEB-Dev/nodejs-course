9fd607ba046c617dc9fb8bc51702abdee77cade2
AliEB-Dev <aliasgharebrahimzadef@gmail.com>
2026-09-03 14:27:35 +0330

    End of Season 7

--- 07 - The Model View Controller (MVC)/work with ejs/models/product.js
+++ 07 - The Model View Controller (MVC)/work with ejs/models/product.js
@@ -1,38 +1,33 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(
+        path.dirname(process.mainModule.filename),
+        'data',
+        'products.json'
+    );
 
+const getProductFormFile = cb => {
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
 module.exports = class Product{
     constructor(t){
         this.title = t;
     }
     save(){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p,(err , fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFormFile(products => {
             products.push(this);
             fs.writeFile(p,JSON.stringify(products) , (err) => {
                 console.log(err);
-            })
-        })
+            });
+        });
     }
     static fetchAll(cb){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p,(err,fileContent)=>{
-            if(err){
-                return cb([]);
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductFormFile(cb);
     }
 }
