22521f27ed602e2efe7655069b2420a837c544fa
AliEB-Dev <aliasgharebrahimzadef@gmail.com>
2026-09-06 14:38:02 +0330

    Add navigation, routes, product storage, and product display

--- 08 - Enhancing the App/models/product.js
+++ 08 - Enhancing the App/models/product.js
@@ -15,9 +15,13 @@ const getProductFormFile = cb => {
         }
     });
 }
+
 module.exports = class Product{
-    constructor(t){
-        this.title = t;
+    constructor(title,imageUrl,description,price){
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
     save(){
         getProductFormFile(products => {
--- 08 - Enhancing the App/routes/admin.js
+++ 08 - Enhancing the App/routes/admin.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-const ProductController = require('../controllers/products');
+const AdminController = require('../controllers/admin');
 
 
-router.get('/add-product',ProductController.getAddProduct)
-router.use('/add-product',ProductController.postAddProducts)
+router.get('/add-product',AdminController.getAddProduct);
+router.get('/products',AdminController.getProducts);
+router.post('/add-product',AdminController.postAddProducts)
 
 exports.router = router;
