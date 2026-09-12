92783798b8a9c8149109be6d69a1c6432db08e13
AliEB-Dev <aliasgharebrahimzadef@gmail.com>
2026-09-12 23:57:38 +0330

    008 Passing Data with POST Requests

--- 09 - Dynamic Routes & Advanced Models/controllers/shop.js
+++ 09 - Dynamic Routes & Advanced Models/controllers/shop.js
@@ -35,6 +35,13 @@ exports.getCart = (req,res,next)=>{
     });
 };
 
+exports.postCart = (req,res,next)=>{
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect("/cart")
+
+}
+
 exports.getCheckout = ()=>{
     res.render('shop/checkout',{
         path:'/checkout',
